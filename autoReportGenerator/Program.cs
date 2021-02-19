7399bb3ec728b560f8077a8dafc350dc9064eb29
Dylan Anderson <dylana1998@gmail.com>
2021-02-19 14:17:58 -0800

    Inserting text from text file into heading.

--- autoReportGenerator/Program.cs
+++ autoReportGenerator/Program.cs
@@ -15,7 +15,22 @@ namespace autoReportGenerator
             doc.Activate();
 
             //writing text to document
-            doc.Content.Text += "Hello World!";
+            doc.Content.Text += "First bit of text\n";
+
+
+            //read from a text file
+            string text = System.IO.File.ReadAllText(@"test.txt");
+            System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+
+            Word.Paragraph paragraph1 = doc.Content.Paragraphs.Add();
+            String heading1 = "Heading 1";
+            paragraph1.Range.Text = "4.3.1";
+            paragraph1.Range.set_Style(heading1);
+            paragraph1.Range.InsertParagraphAfter();
+
+            Word.Paragraph p2 = doc.Content.Paragraphs.Add();
+            p2.Range.Text = text;
+
 
             //save
             doc.SaveAs2("test.docx");
